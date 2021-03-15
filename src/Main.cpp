cd4a2e58eb1631df8de8263a3c669e8641c49121
arend95 <71379402+arend95@users.noreply.github.com>
2021-03-15 18:25:47 +0100

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,10 +2,10 @@
 
 int main(int argc, char *argv[]) {
     Correlator cr;
-
-    cr.CorrDD();
-    cr.CorrRR();
-    cr.CorrDR();
+    
+    cr.Corr12(cr.XD, cr.YD, cr.XD, cr.YD, cr.DD);
+    cr.Corr12(cr.XR, cr.YR, cr.XR, cr.YR, cr.RR);
+    cr.Corr12(cr.XD, cr.YD, cr.XR, cr.YR, cr.DR);
     
     cr.CalcChi();
     cr.WriteData();
